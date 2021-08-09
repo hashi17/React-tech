7c6a2787e06b27ec608d953d64a5dccac1464b70
hashi17 <h.mabo3@gmail.com>
2021-08-09 18:16:32 +0900

    Atomic Design (atom molecule)

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
-import { useMemo } from "react";
 import "./styles.css";
 
 // ルーティングセクション
-import { BrowserRouter, Link } from "react-router-dom";
-import { Router } from "./router/Router";
+import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
+import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
+import { SerachInput } from "./components/molecules/SearchInput";
 
 export default function App() {
-  console.log("App");
-  // 変数のメモ化。複雑な計算を何度もやりたくない場合
-  const temp = useMemo(() => 1 + 3, []);
-  console.log(temp);
-
   return (
-    <BrowserRouter>
-      <div className="App">
-        <Link to="/">Home</Link>
-        <br />
-        <Link to="/page1">Page1</Link>
-        <br />
-        <Link to="/page2">Page2</Link>
-      </div>
-      <Router />
-    </BrowserRouter>
+    <div className="App">
+      <PrimaryButton>テスト</PrimaryButton>
+      <SecondaryButton>検索</SecondaryButton>
+      <SerachInput />
+    </div>
   );
 }
---
+++ src/components/molecules/SearchInput.jsx
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+import { PrimaryButton } from "../atoms/button/PrimaryButton";
+import { Input } from "../atoms/input/Input";
+
+export const SerachInput = () => {
+  return (
+    <div>
+      <SContainer>
+        <Input placeholder="検索条件を入力" />
+        <SButtonWrappr>
+          <PrimaryButton>検索</PrimaryButton>
+        </SButtonWrappr>
+      </SContainer>
+    </div>
+  );
+};
+
+const SContainer = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const SButtonWrappr = styled.div`
+  padding-left: px;
+`;
