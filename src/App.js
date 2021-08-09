ae627bdd6194224146a1c62ffbb7b0d9daf59376
hashi17 <h.mabo3@gmail.com>
2021-08-09 19:12:51 +0900

    Atomic Design Organism

--- src/App.js
+++ src/App.js
@@ -4,6 +4,18 @@ import "./styles.css";
 import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
 import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
 import { SerachInput } from "./components/molecules/SearchInput";
+import { UserCard } from "./components/organisms/user/UserCard";
+
+const user = {
+  image: "https://source.unsplash.com/NE0XGVKTmcA",
+  name: "はっしー",
+  email: "1111@aaa.com",
+  phone: "045-xxx-xxx",
+  company: {
+    name: "〇〇株式会社"
+  },
+  website: "https://giigle.com"
+};
 
 export default function App() {
   return (
@@ -11,6 +23,7 @@ export default function App() {
       <PrimaryButton>テスト</PrimaryButton>
       <SecondaryButton>検索</SecondaryButton>
       <SerachInput />
+      <UserCard user={user} />
     </div>
   );
 }
