055c8f219a1f644c55e9f461089e95e78c4e8224
hashi17 <h.mabo3@gmail.com>
2021-08-09 10:01:25 +0900

    ネストしたルーティング

--- src/App.js
+++ src/App.js
@@ -2,15 +2,18 @@ import { useState, useCallback, useMemo } from "react";
 import { ChildArea } from "./components/ChildArea";
 import { CssModules } from "./components/CssModules";
 import { Emotion } from "./components/Emotion";
-import { Home } from "./components/Home";
 import { InlineStyle } from "./components/InlineStyle";
-import { Page1 } from "./components/Page1";
-import { Page2 } from "./components/Page2";
 import { StyledComponents } from "./components/StyledComponents";
 import { StyledJsx } from "./components/StyledJsx";
 import "./styles.css";
 
+// ルーティングセクション
 import { BrowserRouter, Link, Switch, Route } from "react-router-dom";
+import { Home } from "./Home";
+import { Page1 } from "./Page1";
+import { Page2 } from "./Page2";
+import { Page1DetailA } from "./Page1DetailA";
+import { Page1DetailB } from "./Page1DetailB";
 
 export default function App() {
   const [text, setText] = useState("");
@@ -39,9 +42,23 @@ export default function App() {
         <Route exact path="/">
           <Home />
         </Route>
-        <Route path="/page1">
-          <Page1 />
-        </Route>
+        <Route
+          path="/page1"
+          render={({ match: { url } }) => (
+            <Switch>
+              {console.log(url)}
+              <Route exact path={url}>
+                <Page1 />
+              </Route>
+              <Route path={`${url}/detailA`}>
+                <Page1DetailA />
+              </Route>
+              <Route path={`${url}/detailB`}>
+                <Page1DetailB />
+              </Route>
+            </Switch>
+          )}
+        />
         <Route path="/page2">
           <Page2 />
         </Route>
--- src/components/Page1.jsx
+++
@@ -1,7 +0,0 @@
-export const Page1 = () => {
-  return (
-    <div>
-      <h1>Page1です</h1>
-    </div>
-  );
-};
